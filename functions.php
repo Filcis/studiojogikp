70b88fef897c03275ae82a20c0d067a737b62a16
Filip Ciślak <cislakfilip@gmail.com>
2016-09-13 16:22:45 +0200

    filtr dodający opis do linków na stronie głównej
    tymczasowo jako <span>

--- functions.php
+++ functions.php
@@ -179,3 +179,13 @@ function remove_filter_from_menus( $nav, $args ) {
     remove_filter( 'wp_setup_nav_menu_item', 'filter_menu_items' );
     return $nav;
 }
+
+function prefix_nav_description( $item_output, $item, $depth, $args ) {
+    if (is_front_page()){
+    if ( !empty( $item->description ) ) {
+        $item_output = str_replace( $args->link_after . '</a>', '<span class="menu-item-description">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output );
+    }
+    }
+    return $item_output;
+    }
+add_filter( 'walker_nav_menu_start_el', 'prefix_nav_description', 10, 4 );
