f4579a92e7ce0d19326145b2cccac47cfad3bc32
Filip Ciślak <cislakfilip@gmail.com>
2016-09-15 14:36:48 +0200

    logo zamiast tytułu, szablon na całą szerokość, okruszki

--- header.php
+++ header.php
@@ -27,20 +27,8 @@
             </a>
             <header id="masthead" class="site-header" role="banner">
                 <div class="main-nav-wrapper clear">
-                    <div class="site-branding">
-                        <?php
-			if ( is_front_page() && is_home() ) : ?>
-                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                            <?php else : ?>
-                                <p class="site-title">
-                                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                                        <?php bloginfo( 'name' ); ?>
-                                    </a>
-                                </p>
-                                <?php
-			endif;
-?>
-                    </div>
+                    <div class="site-branding"> <a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                                <img alt="<?php bloginfo( 'name' ); ?>" src="<?php echo get_template_directory_uri() . '/assets/logo.jpg' ?>"></a> </div>
                     <!-- .site-branding -->
                     <nav id="site-navigation" class="main-navigation" role="navigation">
                         <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
--- page-templates/page_full-width.php
+++ page-templates/page_full-width.php
@@ -1,20 +1,15 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: Full Width Page
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package studiojogikp
- */
+ *
 
 /*
 Go to first child if any
 */
+
 $childpages = get_pages("child_of=".$post->ID."&sort_column=menu_order");
 if ($childpages) {
 $firstchild = $childpages[0];
@@ -22,7 +17,7 @@ wp_redirect(get_permalink($firstchild->ID));
 }
 
 get_header(); ?>
-    <div id="primary" class="content-area">
+    <div id="primary" class="content-area-full">
         <main id="main" class="site-main" role="main">
             <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
                 <?php if(function_exists('bcn_display'))
@@ -36,9 +31,6 @@ get_header(); ?>
 				get_template_part( 'template-parts/content', 'page' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
 
 			endwhile; // End of the loop.
 			?>
@@ -47,5 +39,4 @@ get_header(); ?>
     </div>
     <!-- #primary -->
     <?php
-get_sidebar();
 get_footer();
