af11296eb5c26f526e0a59f86184597775cae4d6
Filip Ciślak <cislakfilip@gmail.com>
2016-10-03 16:39:09 +0200

    style dla nawigacji -- więcej kontrastu

--- header-studiojogikp.php
+++ header-studiojogikp.php
@@ -1,16 +1,18 @@
 <header id="masthead" class="site-header" role="banner">
-    <div class="main-nav-wrapper clear">
-        <div class="site-branding"> <a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                                <img alt="<?php bloginfo( 'name' ); ?>" src="<?php echo get_template_directory_uri() . '/assets/logo.jpg' ?>"></a> </div>
-        <!-- .site-branding -->
-        <nav id="site-navigation" class="main-navigation" role="navigation">
-            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-        </nav>
-        <nav id="site-navigation-mobile" class="mobile-navigation" role="navigation">
-            <?php wp_nav_menu( array( 'theme_location' => 'mobile', 'menu_id' => 'mobile-menu' ) ); ?>
-        </nav>
+    <div class="fullwidth-nav-wrapper">
+        <div class="main-nav-wrapper clear">
+            <div class="site-branding"> <a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                                <img alt="<?php bloginfo( 'name' ); ?>" src="<?php echo get_template_directory_uri() . '/assets/logo-negatyw.png' ?>"></a> </div>
+            <!-- .site-branding -->
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+            </nav>
+            <nav id="site-navigation-mobile" class="mobile-navigation" role="navigation">
+                <?php wp_nav_menu( array( 'theme_location' => 'mobile', 'menu_id' => 'mobile-menu' ) ); ?>
+            </nav>
+        </div>
     </div>
-    <!-- #site-navigation -->
+    <!-- #site-navigation kafelki -->
     <div class="secondary-nav-wrapper clear">
         <nav id="secondary-site-navigation" class="tiled-navigation" role="navigation">
             <?php wp_nav_menu( array( 'theme_location' => 'secondary', 'menu_id' => 'secondary-menu-tiles' ) ); ?>
